c838c007acb1ccb259dfd99305bd4ce74891bd9a
chalunton ketkaew <m20471chalunton@gmail.com>
2020-07-30 18:08:46 +0700

    try2
    try2
    
    Co-Authored-By: kchaluntorn <48791544+kchaluntorn@users.noreply.github.com>

---
+++ project01/project01/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+const session = require("express-session");
+const path = require("path");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+    login: false,
+  })
+);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "views")));
+
+const PORT = process.env.port || 7777;
+app.listen(PORT, function (err) {
+  if (!err) {
+    console.log(`App Listen in http://localhost:${PORT}`);
+  } else {
+    console.log(`Can't Listen in http://localhost:${PORT} err ${err}`);
+  }
+});
