01ff4a53c88a57b19c713dab69c7a072d92f49c6
s6002041620055 <48791737+aumcontrol@users.noreply.github.com>
2020-08-06 13:19:02 +0700

    fix routes

--- project01/project01/routes/auth.js
+++ project01/project01/routes/auth.js
@@ -2,7 +2,9 @@ const login = require("../controller/login")
 
 module.exports = (app) => {
     app.get("/login", login);
-    app.post("/login");
+    app.post("/login", function(req, res) {
+        console.log(req.body.user)
+    });
     app.get("/register");
     app.post("/register");
 };
