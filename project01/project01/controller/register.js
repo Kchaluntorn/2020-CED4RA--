f9fe66e9c0c49d6de0a710690a0a27cbb6d72cfa
s6002041620055 <48791737+aumcontrol@users.noreply.github.com>
2020-08-06 14:30:47 +0700

    update auth

--- project01/project01/controller/register.js
+++ project01/project01/controller/register.js
@@ -1,37 +1,44 @@
 const db = require("../db/connect");
+const crypto = require('crypto');
+const bcrypt = require('bcrypt');
+const md5 = require('md5')
 
 const register = (req, res) => {
-  res.send("register");
+    res.send("register");
 };
 const postRegister = (req, res) => {
-  const name = req.body.name;
-  const surname = req.surname.surname;
-  const email = req.body.email;
-  const conpass = req.body.compass;
-  const gitname = req.body.gitname;
-  const pass = req.body.pass;
+    const name = req.body.name;
+    const surname = req.body.surname;
+    const email = req.body.email;
+    const conpass = req.body.conpass;
+    const gitname = req.body.gitname;
+    const pass = req.body.pass;
 
-  if (pass != conpass) {
-    res.render("Register", {
-      massage: "Password and ConfirmPassword not match",
-    });
-  } else {
-    function hashPassword(password) {
-      var salt = crypto.randomBytes(128).toString("base64");
-      var iterations = 10000;
-      var newpass = salt + "$" + password + "iloveis";
-      return newpass;
+    if (pass == conpass) {
+        function hashPassword(password) {
+            var salt = crypto.randomBytes(20).toString("base64");
+            var iterations = 10000;
+
+            var mykey = crypto.createCipher('aes-128-cbc', pass);
+            var mystr = mykey.update('abc', 'utf8', 'hex')
+            mystr += mykey.final('hex');
+
+            var newpass = salt + mystr;
+            return newpass;
+        }
+        const newpass = hashPassword(pass);
+
+        const qt = `insert into user (username,email,password,status,name,surname,github) values ('${email}','${email}','${pass}',0,'${name}','${surname}','${gitname}')`;
+        db().query(qt, (err, rs) => {
+            if (err) {
+                console.log(err);
+            }
+            res.redirect("/login");
+        });
+
+    } else {
+        res.redirect('/registerworng')
     }
-    const newpass = hashPassword(pass);
-    console.log(newpass);
-    // const qt = `insert into user () values ()`;
-    // db().query(qt, (err, rs) => {
-    //   if (err) {
-    //     console.log(err);
-    //   }
-    //   res.redirect("/login");
-    // });
-  }
 };
 module.exports = register;
 module.exports.postRegister = postRegister;
--- project01/project01/routes/auth.js
+++ project01/project01/routes/auth.js
@@ -2,8 +2,10 @@ const login = require("../controller/login");
 const register = require("../controller/register");
 
 module.exports = (app) => {
-  app.get("/login", login);
-  app.post("/login", login.postLogin);
-  app.get("/register", register);
-  app.post("/register", register.postRegister);
+    app.get("/login", login);
+    app.post("/login", login.postLogin);
+    app.get("/register", register);
+    app.post("/register", register.postRegister);
+
+
 };
