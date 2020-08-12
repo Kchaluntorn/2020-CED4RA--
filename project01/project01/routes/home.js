c7594de0a255268be730ee7b8308bb192adb1105
chalunton ketkaew <m20471chalunton@gmail.com>
2020-08-12 15:54:29 +0700

    1234

--- project01/project01/routes/home.js
+++ project01/project01/routes/home.js
@@ -3,13 +3,17 @@ const db = require("../db/connect");
 module.exports = (app) => {
   app.get("/index", (req, res) => {
     if (req.session.loggedin == true) {
-      const qt = "select * from user";
+      const qt = "select * from user where status=0";
       db().query(qt, (err, rs) => {
         if (err) {
           console.log(err);
         }
         console.log(rs);
-        res.render("datacenter", { name: req.session.name, rs: rs });
+        res.render("datacenter", {
+          name: req.session.name,
+          rs: rs,
+          status: req.session.status,
+        });
       });
     } else {
       res.redirect("/login");
